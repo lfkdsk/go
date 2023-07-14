63475a0e9ae96410b6c79b4db3dd50201555f9f9
lfkdsk <liufengkai@bytedance.com>
2023-07-14 17:58:21 +0800

    feat: change lattice def & add local range.

--- src/cmd/compile/internal/ssa/sccp.go
+++ src/cmd/compile/internal/ssa/sccp.go
@@ -41,14 +41,64 @@ import (
 
 // Three level lattice holds compile time knowledge about SSA value
 const (
-	top      int8 = iota // undefined
-	constant             // constant
-	bottom               // not a constant
+	top        int8 = iota // undefined
+	valueRange             // constant
+	bottom                 // not a constant
 )
 
 type lattice struct {
-	tag int8   // lattice type
-	val *Value // constant value
+	tag   int8   // lattice type
+	lower *Value // lower value
+	upper *Value // upper value
+}
+
+func (l *lattice) isConstant() bool {
+	if l.tag != valueRange {
+		return false
+	}
+
+	if l.lower == nil || l.upper == nil {
+		return false
+	}
+
+	if l.lower == l.upper {
+		return true
+	}
+
+	return l.lower.Op == l.upper.Op && l.lower.AuxInt == l.upper.AuxInt
+}
+
+func (l *lattice) isRange() bool {
+	return l.tag == valueRange && !l.isConstant()
+}
+
+func (l *lattice) ConstValue() *Value {
+	if !l.isConstant() {
+		return nil
+	}
+	return l.lower
+}
+
+func (l *lattice) ValueString() string {
+	var tag, lower, upper string
+	switch l.tag {
+	case top:
+		tag = "top"
+		break
+	case bottom:
+		tag = "bottom"
+		break
+	case valueRange:
+		tag = "range"
+		if l.lower != nil {
+			lower = l.lower.auxString()
+		}
+		if l.upper != nil {
+			upper = l.upper.auxString()
+		}
+		break
+	}
+	return fmt.Sprintf("%s[%s,%s]", tag, lower, upper)
 }
 
 type worklist struct {
@@ -58,8 +108,12 @@ type worklist struct {
 	visited      map[Edge]bool       // visited edges
 	latticeCells map[*Value]lattice  // constant lattices
 	defUse       map[*Value][]*Value // def-use chains for some values
+	useDef       map[*Value]*Block
 	defBlock     map[*Value][]*Block // use blocks of def
 	visitedBlock []bool              // visited block
+	blockLattice map[*Value]map[*Block]lattice
+	rangeInit    map[*Value]*Value
+	sdom         SparseTree
 }
 
 // sccp stands for sparse conditional constant propagation, it propagates constants
@@ -72,9 +126,13 @@ func sccp(f *Func) {
 	t.visited = make(map[Edge]bool)
 	t.edges = append(t.edges, Edge{f.Entry, 0})
 	t.defUse = make(map[*Value][]*Value)
+	t.useDef = make(map[*Value]*Block)
 	t.defBlock = make(map[*Value][]*Block)
 	t.latticeCells = make(map[*Value]lattice)
 	t.visitedBlock = f.Cache.allocBoolSlice(f.NumBlocks())
+	t.rangeInit = make(map[*Value]*Value)
+	t.blockLattice = make(map[*Value]map[*Block]lattice)
+	t.sdom = f.Sdom()
 	defer f.Cache.freeBoolSlice(t.visitedBlock)
 
 	// build it early since we rely heavily on the def-use chain later
@@ -94,7 +152,7 @@ func sccp(f *Func) {
 				t.visitedBlock[dest.ID] = true
 				for _, val := range dest.Values {
 					if val.Op == OpPhi || !destVisited {
-						t.visitValue(val)
+						t.visitValue(val, dest)
 					}
 				}
 				// propagates constants facts through CFG, taking condition test
@@ -108,7 +166,7 @@ func sccp(f *Func) {
 		if len(t.uses) > 0 {
 			use := t.uses[0]
 			t.uses = t.uses[1:]
-			t.visitValue(use)
+			t.visitValue(use, nil)
 			continue
 		}
 		break
@@ -131,17 +189,18 @@ func equals(a, b lattice) bool {
 	if a.tag != b.tag {
 		return false
 	}
-	if a.tag == constant {
+	if a.isConstant() && b.isConstant() {
 		// The same content of const value may be different, we should
 		// compare with auxInt instead
-		v1 := a.val
-		v2 := b.val
+		v1 := a.ConstValue()
+		v2 := b.ConstValue()
 		if v1.Op == v2.Op && v1.AuxInt == v2.AuxInt {
 			return true
 		} else {
 			return false
 		}
 	}
+	// TODO range compare.
 	return true
 }
 
@@ -222,14 +281,44 @@ func possibleConst(val *Value) bool {
 	}
 }
 
-func (t *worklist) getLatticeCell(val *Value) lattice {
+func (t *worklist) searchThroughBlock(val *Value, b *Block) *lattice {
+	if b == nil {
+		return nil
+	}
+
+	_, exist := t.blockLattice[val]
+	if !exist {
+		return nil
+	}
+
+	sb := b
+	l, exist := t.blockLattice[val][sb]
+	if exist {
+		return &l
+	}
+	for {
+		if !exist && sb != nil {
+			l, exist = t.blockLattice[val][sb]
+			sb = t.sdom.Parent(sb)
+			continue
+		}
+		break
+	}
+	return nil
+}
+
+func (t *worklist) getLatticeCell(val *Value, block *Block) lattice {
 	if !possibleConst(val) {
 		// they are always worst
-		return lattice{bottom, nil}
+		return lattice{bottom, nil, nil}
+	}
+	blt := t.searchThroughBlock(val, block)
+	if blt != nil {
+		return *blt
 	}
 	lt, exist := t.latticeCells[val]
 	if !exist {
-		return lattice{top, nil} // optimistically for un-visited value
+		return lattice{top, nil, nil} // optimistically for un-visited value
 	}
 	return lt
 }
@@ -238,6 +327,24 @@ func isConst(val *Value) bool {
 	switch val.Op {
 	case OpConst64, OpConst32, OpConst16, OpConst8,
 		OpConstBool, OpConst32F, OpConst64F:
+		return true
+	default:
+		return false
+	}
+}
+
+func isCompare(op Op) bool {
+	switch op {
+	case // compare
+		OpEq64, OpEq32, OpEq16, OpEq8,
+		OpEq32F, OpEq64F,
+		OpLess64, OpLess32, OpLess16, OpLess8,
+		OpLess64U, OpLess32U, OpLess16U, OpLess8U,
+		OpLess32F, OpLess64F,
+		OpLeq64, OpLeq32, OpLeq16, OpLeq8,
+		OpLeq64U, OpLeq32U, OpLeq16U, OpLeq8U,
+		OpLeq32F, OpLeq64F,
+		OpEqB, OpNeqB:
 		return true
 	default:
 		return false
@@ -259,6 +366,7 @@ func (t *worklist) buildDefUses() {
 						t.defUse[arg] = make([]*Value, 0, arg.Uses)
 					}
 					t.defUse[arg] = append(t.defUse[arg], val)
+					t.useDef[val] = block
 				}
 			}
 		}
@@ -266,6 +374,75 @@ func (t *worklist) buildDefUses() {
 			// for control values that can become constants, find their use blocks
 			if possibleConst(ctl) {
 				t.defBlock[ctl] = append(t.defBlock[ctl], block)
+				if isCompare(ctl.Op) {
+					var str string
+					for _, s := range ctl.Args {
+						str += s.LongString() + " , "
+					}
+					fmt.Printf("cv: %s: %s \n", ctl.LongString(), str)
+					for _, arg := range ctl.Args {
+						if !isConst(arg) {
+							t.rangeInit[arg] = ctl
+							t.blockLattice[arg] = make(map[*Block]lattice)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for _, block := range t.f.Blocks {
+		for _, val := range block.Values {
+			for _, arg := range val.Args {
+				check, exist := t.rangeInit[arg]
+				_, localtexist := t.blockLattice[arg][block]
+				if exist && isCompare(check.Op) && !localtexist {
+					index := 0
+					if check.Args[1] == arg {
+						index = 1
+					}
+					or := getBranch(t.sdom, t.sdom.Parent(block), block)
+					if or == positive || or == negative {
+						left := index == 0 && possibleConst(check.Args[1])
+						right := index == 1 && possibleConst(check.Args[0])
+						switch check.Op {
+						case // compare
+							OpEq64, OpEq32, OpEq16, OpEq8,
+							OpEq32F, OpEq64F:
+							if left {
+								t.blockLattice[arg][block] = lattice{valueRange, check.Args[1], check.Args[1]}
+							} else if right {
+								t.blockLattice[arg][block] = lattice{valueRange, check.Args[0], check.Args[0]}
+							}
+							break
+						case OpLess64, OpLess32, OpLess16, OpLess8,
+							OpLess64U, OpLess32U, OpLess16U, OpLess8U,
+							OpLess32F, OpLess64F:
+							fallthrough
+						case OpLeq64, OpLeq32, OpLeq16, OpLeq8,
+							OpLeq64U, OpLeq32U, OpLeq16U, OpLeq8U,
+							OpLeq32F, OpLeq64F:
+							if left {
+								if or == positive {
+									t.blockLattice[arg][block] = lattice{valueRange, nil, check.Args[1]}
+								} else {
+									t.blockLattice[arg][block] = lattice{valueRange, check.Args[1], nil}
+								}
+							} else if right {
+								if or == positive {
+									t.blockLattice[arg][block] = lattice{valueRange, check.Args[0], nil}
+								} else {
+									t.blockLattice[arg][block] = lattice{valueRange, nil, check.Args[0]}
+								}
+							}
+							break
+						}
+						l, exist := t.blockLattice[arg][block]
+						if exist {
+							fmt.Printf("test val %s, %s \n", arg.String(), l.ValueString())
+						}
+					}
+				}
 			}
 		}
 	}
@@ -289,8 +466,8 @@ func (t *worklist) addUses(val *Value) {
 }
 
 // meet meets all of phi arguments and computes result lattice
-func (t *worklist) meet(val *Value) lattice {
-	optimisticLt := lattice{top, nil}
+func (t *worklist) meet(val *Value, block *Block) lattice {
+	optimisticLt := lattice{top, nil, nil}
 	for i := 0; i < len(val.Args); i++ {
 		edge := Edge{val.Block, i}
 		// If incoming edge for phi is not visited, assume top optimistically.
@@ -299,19 +476,19 @@ func (t *worklist) meet(val *Value) lattice {
 		// Top participates in meet() but does not affect the result, so here
 		// we will ignore Top and only take other lattices into consideration.
 		if _, exist := t.visited[edge]; exist {
-			lt := t.getLatticeCell(val.Args[i])
-			if lt.tag == constant {
+			lt := t.getLatticeCell(val.Args[i], block)
+			if lt.isConstant() {
 				if optimisticLt.tag == top {
 					optimisticLt = lt
 				} else {
 					if !equals(optimisticLt, lt) {
 						// ConstantA ∩ ConstantB = Bottom
-						return lattice{bottom, nil}
+						return lattice{bottom, nil, nil}
 					}
 				}
 			} else if lt.tag == bottom {
 				// Bottom ∩ any = Bottom
-				return lattice{bottom, nil}
+				return lattice{bottom, nil, nil}
 			} else {
 				// Top ∩ any = any
 			}
@@ -355,31 +532,37 @@ func computeLattice(f *Func, val *Value, args ...*Value) lattice {
 	matched := rewriteValuegeneric(constValue)
 	if matched {
 		if isConst(constValue) {
-			return lattice{constant, constValue}
+			return lattice{valueRange, constValue, constValue}
 		}
 	}
 	// Either we can not match generic rules for given value or it does not
 	// satisfy additional constraints(e.g. divide by zero), in these cases, clean
 	// up temporary value immediately in case they are not dominated by their args.
 	constValue.reset(OpInvalid)
-	return lattice{bottom, nil}
+	return lattice{bottom, nil, nil}
 }
 
-func (t *worklist) visitValue(val *Value) {
+func (t *worklist) visitValue(val *Value, block *Block) {
 	if !possibleConst(val) {
 		// fast fail for always worst Values, i.e. there is no lowering happen
 		// on them, their lattices must be initially worse Bottom.
 		return
 	}
 
-	oldLt := t.getLatticeCell(val)
+	oldLt := t.getLatticeCell(val, nil)
+	s := "nil"
+	if block != nil {
+		s = block.String()
+	}
+	fmt.Printf("%s , %s b: %s \n", val.LongString(), oldLt.ValueString(), s)
 	defer func() {
 		// re-visit all uses of value if its lattice is changed
-		newLt := t.getLatticeCell(val)
+		newLt := t.getLatticeCell(val, nil)
 		if !equals(newLt, oldLt) {
 			if int8(oldLt.tag) > int8(newLt.tag) {
-				t.f.Fatalf("Must lower lattice\n")
+				t.f.Fatalf("Must lower lattice %s %s -> %s \n", val.LongString(), oldLt.ValueString(), newLt.ValueString())
 			}
+			fmt.Printf("change: %s , %s b: %s \n", val.LongString(), newLt.ValueString(), s)
 			t.addUses(val)
 		}
 	}()
@@ -388,13 +571,13 @@ func (t *worklist) visitValue(val *Value) {
 	// they are constant values, aren't they?
 	case OpConst64, OpConst32, OpConst16, OpConst8,
 		OpConstBool, OpConst32F, OpConst64F: //TODO: support ConstNil ConstString etc
-		t.latticeCells[val] = lattice{constant, val}
+		t.latticeCells[val] = lattice{valueRange, val, val}
 	// lattice value of copy(x) actually means lattice value of (x)
 	case OpCopy:
-		t.latticeCells[val] = t.getLatticeCell(val.Args[0])
+		t.latticeCells[val] = t.getLatticeCell(val.Args[0], block)
 	// phi should be processed specially
 	case OpPhi:
-		t.latticeCells[val] = t.meet(val)
+		t.latticeCells[val] = t.meet(val, block)
 	// fold 1-input operations:
 	case
 		// negate
@@ -419,14 +602,15 @@ func (t *worklist) visitValue(val *Value) {
 		OpIsNonNil,
 		// not
 		OpNot:
-		lt1 := t.getLatticeCell(val.Args[0])
+		lt1 := t.getLatticeCell(val.Args[0], block)
 
-		if lt1.tag == constant {
+		if lt1.isConstant() {
 			// here we take a shortcut by reusing generic rules to fold constants
-			t.latticeCells[val] = computeLattice(t.f, val, lt1.val)
+			t.latticeCells[val] = computeLattice(t.f, val, lt1.ConstValue())
 		} else {
-			t.latticeCells[val] = lattice{lt1.tag, nil}
+			t.latticeCells[val] = lattice{lt1.tag, nil, nil}
 		}
+
 	// fold 2-input operations
 	case
 		// add
@@ -465,17 +649,49 @@ func (t *worklist) visitValue(val *Value) {
 		OpAnd8, OpAnd16, OpAnd32, OpAnd64,
 		OpOr8, OpOr16, OpOr32, OpOr64,
 		OpXor8, OpXor16, OpXor32, OpXor64:
-		lt1 := t.getLatticeCell(val.Args[0])
-		lt2 := t.getLatticeCell(val.Args[1])
+		lt1 := t.getLatticeCell(val.Args[0], block)
+		lt2 := t.getLatticeCell(val.Args[1], block)
 
-		if lt1.tag == constant && lt2.tag == constant {
+		if lt1.isConstant() && lt2.isConstant() {
+			fmt.Printf("two lwV: %s upV: %s \n", lt1.ConstValue().auxString(), lt2.ConstValue().auxString())
 			// here we take a shortcut by reusing generic rules to fold constants
-			t.latticeCells[val] = computeLattice(t.f, val, lt1.val, lt2.val)
+			t.latticeCells[val] = computeLattice(t.f, val, lt1.ConstValue(), lt2.ConstValue())
+		} else if lt1.isRange() && lt2.isConstant() {
+			var lwV, upV *Value
+			if lt1.lower != nil {
+				lw := computeLattice(t.f, val, lt1.lower, lt2.ConstValue())
+				if lw.isConstant() {
+					lwV = lw.ConstValue()
+				}
+			}
+			if lt1.upper != nil {
+				up := computeLattice(t.f, val, lt1.upper, lt2.ConstValue())
+				if up.isConstant() {
+					upV = up.ConstValue()
+				}
+			}
+			t.latticeCells[val] = lattice{valueRange, lwV, upV}
+		} else if lt1.isConstant() && lt2.isRange() {
+			var lwV, upV *Value
+			if lt2.lower != nil {
+				lw := computeLattice(t.f, val, lt1.ConstValue(), lt2.lower)
+				if lw.isConstant() {
+					lwV = lw.ConstValue()
+				}
+			}
+			if lt2.upper != nil {
+				up := computeLattice(t.f, val, lt1.ConstValue(), lt2.upper)
+				if up.isConstant() {
+					upV = up.ConstValue()
+				}
+			}
+			fmt.Printf("two lwV: %s upV: %s \n", lwV.String(), upV.String())
+			t.latticeCells[val] = lattice{valueRange, lwV, upV}
 		} else {
 			if lt1.tag == bottom || lt2.tag == bottom {
-				t.latticeCells[val] = lattice{bottom, nil}
+				t.latticeCells[val] = lattice{bottom, nil, nil}
 			} else {
-				t.latticeCells[val] = lattice{top, nil}
+				t.latticeCells[val] = lattice{top, nil, nil}
 			}
 		}
 	default:
@@ -498,19 +714,21 @@ func (t *worklist) propagate(block *Block) {
 		fallthrough // always takes the first branch
 	case BlockPlain:
 		t.edges = append(t.edges, block.Succs[0])
-	case BlockIf, BlockJumpTable:
+	case BlockIf:
+		fallthrough
+	case BlockJumpTable:
 		cond := block.ControlValues()[0]
-		condLattice := t.getLatticeCell(cond)
+		condLattice := t.getLatticeCell(cond, nil)
 		if condLattice.tag == bottom {
 			// we know nothing about control flow, add all branch destinations
 			t.edges = append(t.edges, block.Succs...)
-		} else if condLattice.tag == constant {
+		} else if condLattice.isConstant() {
 			// add branchIdx destinations depends on its condition
 			var branchIdx int64
 			if block.Kind == BlockIf {
-				branchIdx = 1 - condLattice.val.AuxInt
+				branchIdx = 1 - condLattice.ConstValue().AuxInt
 			} else {
-				branchIdx = condLattice.val.AuxInt
+				branchIdx = condLattice.ConstValue().AuxInt
 			}
 			t.edges = append(t.edges, block.Succs[branchIdx])
 		} else {
@@ -553,19 +771,19 @@ func rewireSuccessor(block *Block, constVal *Value) bool {
 func (t *worklist) replaceConst() (int, int) {
 	constCnt, rewireCnt := 0, 0
 	for val, lt := range t.latticeCells {
-		if lt.tag == constant {
+		if lt.isConstant() {
 			if !isConst(val) {
 				if t.f.pass.debug > 0 {
-					fmt.Printf("Replace %v with %v\n", val.LongString(), lt.val.LongString())
+					fmt.Printf("Replace %v with %v\n", val.LongString(), lt.ConstValue().LongString())
 				}
-				val.reset(lt.val.Op)
-				val.AuxInt = lt.val.AuxInt
+				val.reset(lt.ConstValue().Op)
+				val.AuxInt = lt.ConstValue().AuxInt
 				constCnt++
 			}
 			// If const value controls this block, rewires successors according to its value
 			ctrlBlock := t.defBlock[val]
 			for _, block := range ctrlBlock {
-				if rewireSuccessor(block, lt.val) {
+				if rewireSuccessor(block, lt.ConstValue()) {
 					rewireCnt++
 					if t.f.pass.debug > 0 {
 						fmt.Printf("Rewire %v %v successors\n", block.Kind, block)
